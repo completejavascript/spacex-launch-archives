3388124694e7231f6e497b7fc52f55ed8bf5797d
Lam Pham <completejavascript.super@gmail.com>
2022-11-05 14:06:38 +0700

    fix: add loading skeleton to sidebar

--- src/components/LaunchList/index.tsx
+++ src/components/LaunchList/index.tsx
@@ -50,6 +50,34 @@ export default function LaunchList() {
     return Object.entries(mapByYear);
   }, [data, loading]);
 
+  if (loading) {
+    return (
+      <div className="h-full relative overflow-y-scroll px-8">
+        <div className="animate-pulse flex flex-col">
+          <div className="sticky top-0">
+            <div className="h-8 bg-slate-900" />
+            <div className="h-4 bg-slate-900 relative w-full">
+              <div className="rounded-md py-5 px-3 bg-slate-700" />
+            </div>
+            <div className="h-8 bg-gradient-to-b from-slate-900" />
+          </div>
+
+          <div className="mt-5">
+            <div className="bg-slate-700 h-4 rounded-sm mb-4" />
+            <div className="space-y-2">
+              <div className="border-l border-slate-800">
+                <div className="h-3 bg-slate-700 ml-4 rounded-sm" />
+              </div>
+              <div className="border-l border-slate-800">
+                <div className="h-3 bg-slate-700 ml-4 rounded-sm" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="h-full relative overflow-y-scroll px-8">
       <div className="sticky top-0">
@@ -70,9 +98,7 @@ export default function LaunchList() {
           return (
             <ul key={yearStr}>
               <li className="mb-8">
-                <h5 className="mb-8 lg:mb-3 font-semibold text-slate-200">
-                  {yearStr}
-                </h5>
+                <h5 className="mb-3 font-semibold text-slate-200">{yearStr}</h5>
                 <ul className="space-y-2 border-l border-slate-800">
                   {listLaunch.map((launch) => {
                     return (
